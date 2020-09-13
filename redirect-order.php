aa236afb52d7eda4d5fa1e1e446b1ca4acab5a07
Charles <charlesbel30@gmail.com>
2020-09-13 06:46:28 +0200

    Update redirect-order.php

--- redirect-order.php
+++ redirect-order.php
@@ -5,39 +5,30 @@ $app_id = 'YOUR APP ID';
 $app_pass = 'YOUR APP PASS';
 $store_url = 'YOUR SHOP MYSHOPIFY DOMAIN';
 
-$url = 'https://' . $app_id . '@' . $store_url . '/admin/api/2020-04/customers.json?email=' . $_GET['email'];
+$url = 'https://' . $app_id . '@' . $store_url . '/admin/api/2020-04/orders.json?status=any';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'X-Shopify-Access-Token: ' . $app_pass));
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-$response = curl_exec($ch);
-curl_close($ch);
-
-$customer = json_decode($response, true);
-
-$last_order_id = $customer["customers"][0]["last_order_id"];
-
-
-$url = 'https://' . $app_id . '@' . $store_url . '/admin/api/2020-04/orders/' . $last_order_id . '.json';
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'X-Shopify-Access-Token: ' . $app_pass));
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Shopify-Access-Token: ' . $app_pass));
 $response = curl_exec($ch);
 curl_close($ch);
 
 $order = json_decode($response, true);
 
-$link = $order["order"]["order_status_url"];
+$order = $order["orders"];
 
+for ($i = 1; $i <= count($order); $i++) {
 
-header('location:' . $link);
+    if($order[$i-1]['email'] == $_GET['email']) {
+        $link = $order[$i-1]["order_status_url"];
+        break;
+    }
+
+}
+
+header('Location: ' . $link);
 exit();
 
 ?>
